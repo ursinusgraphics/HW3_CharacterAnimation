4ad0ff930e2b5012660c9a59480a9edc3abef94d
Chris Tralie <chris.tralie@gmail.com>
2024-10-06 15:57:23 -0400

    Updating exportBones script to be exactly as it came out in the video

--- rigs/exportBones.py
+++ rigs/exportBones.py
@@ -3,22 +3,26 @@ import json
 
 scene = bpy.data.scenes["Scene"]
 armature = scene.objects["Armature"]
-
 joints = {}
-for bone in armature.pose.bones:
-    p = bone.tail
-    children = [c.name for c in bone.children]
-    joints[bone.name] = dict(pos=[p.x, p.z, -p.y], children=children, weights={})
-#print(joints)
 
-mesh = scene.objects["tr_reg_090"]
-N = len(mesh.data.vertices)
-for key, vg in mesh.vertex_groups.items():
-    for i in range(N):
+for bone in armature.pose.bones:
+    name = bone.name
+    pos = bone.tail
+    joints[name] = dict(pos=(pos.x, pos.z, -pos.y))
+    joints[name]["children"] = []
+    for c in bone.children:
+        joints[name]["children"].append(c.name)
+
+
+mesh = scene.objects["homer"]
+n_vertices = len(mesh.data.vertices)
+for bone, group in mesh.vertex_groups.items():
+    joints[bone]["weights"] = {}
+    for i in range(n_vertices):
         try:
-            w = vg.weight(i)
-            joints[key]["weights"][i] = w
+            w = group.weight(i)
+            joints[bone]["weights"][i] = w
         except:
             pass
-
-json.dump(joints, open("tr_reg_090.json", "w"))
+        
+json.dump(joints, open("homer.json", "w"))
